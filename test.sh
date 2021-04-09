3aa7e56c85df0c7c9ecc896d37d239ccd20b5e03
Jesus Carpintero <jesus.carpintero@adevinta.com>
2021-04-09 13:19:05 +0200

    Update tests

--- test.sh
+++ test.sh
@@ -4,10 +4,10 @@ find ./stable -not -name vulcan -maxdepth 1 -mindepth 1 -type d -print -exec hel
 helm dep update --skip-refresh ./stable/vulcan
 helm lint ./stable/*
 
-pushd test
+pushd examples
 for f in *.yaml
 do
-  helm template vulcan ../stable/vulcan -f $f > templates/$f
+  helm template vulcan ../stable/vulcan --namespace ns -f $f > templates/$f
   helm template vulcan ../stable/vulcan -f $f --debug| kubeval --strict --ignore-missing-schemas
 done
 popd
